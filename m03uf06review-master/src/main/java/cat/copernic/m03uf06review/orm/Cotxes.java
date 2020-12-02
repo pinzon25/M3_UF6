13a5d861ff4eb2bf633dae126af7501490c51187
Ricard Pinzón <riki_net69@hotmail.com>
2020-12-02 21:14:12 +0100

    Add files via upload

---
+++ m03uf06review-master/src/main/java/cat/copernic/m03uf06review/orm/Cotxes.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.copernic.m03uf06review.orm;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author riki
+ */
+public class Cotxes {
+
+    private String Model;
+    private int Potencia_motor;
+    private float Acceleracio_motor;
+    private boolean Origen_europeu;
+    private Date Data_fabricacio;
+
+    /*public Cotxes() {
+        Model = "";
+        Potencia_motor = 0;
+        Acceleracio_motor = 0;
+        Origen_europeu = false;
+        Data_fabricacio = null;
+    }*/
+
+    public Cotxes(String Model, int Potencia_motor, float Acceleracio_motor, boolean Origen_europeu, Date Data_fabricacio) {
+        this.Model = Model;
+        this.Potencia_motor = Potencia_motor;
+        this.Acceleracio_motor = Acceleracio_motor;
+        this.Origen_europeu = Origen_europeu;
+        this.Data_fabricacio = Data_fabricacio;
+    }
+
+    /**
+     * @return the Model
+     */
+    public String getModel() {
+        return Model;
+    }
+
+    /**
+     * @param Model the Model to set
+     */
+    public void setModel(String Model) {
+        this.Model = Model;
+    }
+
+    /**
+     * @return the Potencia_motor
+     */
+    public int getPotencia_motor() {
+        return Potencia_motor;
+    }
+
+    /**
+     * @param Potencia_motor the Potencia_motor to set
+     */
+    public void setPotencia_motor(int Potencia_motor) {
+        this.Potencia_motor = Potencia_motor;
+    }
+
+    /**
+     * @return the Acceleracio_motor
+     */
+    public float getAcceleracio_motor() {
+        return Acceleracio_motor;
+    }
+
+    /**
+     * @param Acceleracio_motor the Acceleracio_motor to set
+     */
+    public void setAcceleracio_motor(float Acceleracio_motor) {
+        this.Acceleracio_motor = Acceleracio_motor;
+    }
+
+    /**
+     * @return the Origen_europeu
+     */
+    public boolean isOrigen_europeu() {
+        return Origen_europeu;
+    }
+
+    /**
+     * @param Origen_europeu the Origen_europeu to set
+     */
+    public void setOrigen_europeu(boolean Origen_europeu) {
+        this.Origen_europeu = Origen_europeu;
+    }
+
+    /**
+     * @return the Data_fabricacio
+     */
+    public Date getData_fabricacio() {
+        return Data_fabricacio;
+    }
+
+    /**
+     * @param Data_fabricacio the Data_fabricacio to set
+     */
+    public void setData_fabricacio(Date Data_fabricacio) {
+        this.Data_fabricacio = Data_fabricacio;
+    }
+
+    @Override
+    public String toString() {
+        return "Cotxes{" + "Model=" + Model + ", Potencia_motor=" + Potencia_motor + ", Acceleracio_motor=" + Acceleracio_motor + ", Origen_europeu=" + Origen_europeu + ", Data_fabricacio=" + Data_fabricacio + '}';
+    }
+
+}
--- m03uf06review-master/src/main/java/cat/copernic/m03uf06review/orm/OrmMain.java
+++ m03uf06review-master/src/main/java/cat/copernic/m03uf06review/orm/OrmMain.java
@@ -5,24 +5,62 @@
  */
 package cat.copernic.m03uf06review.orm;
 
+import cat.copernic.m03uf06review.jdbc.Connexio;
+import cat.copernic.m03uf06review.jdbc.JdbcMain;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * En aquesta secció cal accedir a una taula de MySQL amb un camp de cada tipus:
- * 
+ *
  * int o long, double o float, boolean, char, String i java.sql.Date
- * 
- * Recòrrer el result set i mostrar-lo per la consola com a instancies de la 
+ *
+ * Recòrrer el result set i mostrar-lo per la consola com a instancies de la
  * classe Registre, que tindrà l'estructura de la teva taula.
- * 
- * 
+ *
+ *
  * @author pep
  */
 public class OrmMain {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static ArrayList<Cotxes> ctx = new ArrayList<>();
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        obteCotxes();
+        mostraCotxes();
     }
-    
+
+    private static void obteCotxes() {
+        Connexio c = new Connexio();
+
+        try {
+            String query = "select * from cotxe";
+
+            PreparedStatement st = c.getConexio().prepareStatement(query);
+            ResultSet rs = st.executeQuery();
+
+            while (rs.next()) {
+                Cotxes ct = null;
+                ct = new Cotxes(rs.getString("Model"), rs.getInt("Potencia_motor"), rs.getFloat("Acceleracio_cent"), rs.getBoolean("Origen_europeu"), rs.getDate("Data_fabricacio"));
+                afegeixCotxes(ct);
+            }
+
+        } catch (SQLException ex) {Logger.getLogger(JdbcMain.class.getName()).log(Level.SEVERE, null, ex);}
+    }
+
+    private static ArrayList<Cotxes> afegeixCotxes(Cotxes ct) {
+        ctx.add(ct);
+        return ctx;
+    }
+
+    private static void mostraCotxes() {
+        for (int i = 0; i < ctx.size(); i++) {
+            System.out.println(ctx.get(i));
+        }
+    }
+
 }
