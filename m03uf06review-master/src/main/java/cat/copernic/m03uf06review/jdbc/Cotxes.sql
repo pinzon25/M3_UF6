28ea1b49a7d2538e665243466ef17d95c2f09f4e
Ricard Pinzón <riki_net69@hotmail.com>
2020-12-06 16:31:49 +0100

    Add files via upload

--- m03uf06review-master/src/main/java/cat/copernic/m03uf06review/jdbc/Cotxes.sql
+++ m03uf06review-master/src/main/java/cat/copernic/m03uf06review/jdbc/Cotxes.sql
@@ -9,7 +9,7 @@ CREATE USER 'Ricard'@'localhost' IDENTIFIED BY '1234';
 
 DROP TABLE IF EXISTS Cotxe;
 CREATE TABLE cotxe(
-Model VARCHAR(50),
+Model VARCHAR(50) PRIMARY KEY,
 Potencia_motor INTEGER(4),
 Acceleracio_cent FLOAT,
 Origen_europeu BOOLEAN,
@@ -20,3 +20,5 @@ INSERT INTO Cotxe VALUES('Subaru impreza GT Turbo', 218, 6.3, false, '1993-3-21'
 INSERT INTO Cotxe VALUES('Mitsubishi Lancer EVO 8', 230, 6.1, false, '2005-3-3');
 
 #select * from Cotxe;
+
+SET GLOBAL time_zone = '+3:00'; #El canvi de la configuracio horaria es requerit degut a que sino detecta multiples zones horaries
